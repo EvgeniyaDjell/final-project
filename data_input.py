af60ce28e6f128c45baa8fe356500ee2b79cae02
EvgeniyaDjell <118808667+EvgeniyaDjell@users.noreply.github.com>
2022-11-23 02:33:57 +0300

    Add files via upload

---
+++ data_input.py
@@ -0,0 +1,8 @@
+# Здесь необходимо указать входные данные:
+#     ID пользователя VK, чьи фотографии будут загружаться на яндекс-диск
+#     Токен Яндекс-диска
+#     Количество загружаемых фотографий
+USER_ID = '10748257'
+TOKEN_YANDEX = 'y0_AgAAAABAWNBsAADLWwAAAADSzxd0vQ8Q-uQRSbW694VqHDN5bOHSspY'
+NUMBER_UPLOAD_PHOTO = int(5)
+FOLDER_NAME = 'photo_from_VK'

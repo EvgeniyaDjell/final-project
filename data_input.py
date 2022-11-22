8d1bbe6712e7f4b8711f9217cb375cea3e0553c6
EvgeniyaDjell <118808667+EvgeniyaDjell@users.noreply.github.com>
2022-11-23 02:34:22 +0300

    Update data_input.py

--- data_input.py
+++ data_input.py
@@ -2,7 +2,7 @@
 #     ID пользователя VK, чьи фотографии будут загружаться на яндекс-диск
 #     Токен Яндекс-диска
 #     Количество загружаемых фотографий
-USER_ID = '10748257'
-TOKEN_YANDEX = 'y0_AgAAAABAWNBsAADLWwAAAADSzxd0vQ8Q-uQRSbW694VqHDN5bOHSspY'
+USER_ID = ''
+TOKEN_YANDEX = ''
 NUMBER_UPLOAD_PHOTO = int(5)
 FOLDER_NAME = 'photo_from_VK'
